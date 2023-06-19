184d969c901f078923a2ec45ded23987ad4ef7b1
shanice <shankwony@gmail.com>
2023-06-19 12:03:44 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - Using printf function to print a line in a program
+ * Return: 0(Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
