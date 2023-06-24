e136eb93eb4805e053d0c93d966ce06dc747c932
shanice <shankwony@gmail.com>
2023-06-24 02:44:29 -0700

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _is lower - checks for lowercase character
+ * _islower - checks for lowercase character
  * @c: The character to be checked
  * Return: 1 for lowercase character or @ for anything
  */
